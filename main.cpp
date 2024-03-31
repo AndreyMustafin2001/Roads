541db86e2d7da4600f64c9960ddab194c153813a
AndreyMustafin2001 <andrey.must2001@gmail.com>
2024-03-31 20:01:35 +0400

    changed logic
    Now launches cars in parallel

--- main.cpp
+++ main.cpp
@@ -3,43 +3,29 @@
 #include <mutex>
 #include <deque>
 #include <unistd.h>
+#include <vector>
 using namespace std;
 mutex m;
-void roadA( deque <int> &cars, deque  <int> &directions,deque  <int> &time)
+bool svetafor = 0;
+// A - 0
+// B - 1
+void turnGreen(int roadId)
 {
-    while(true)
-    {
-        sleep(1);
-        lock_guard<mutex> guard(m);
-        if(cars.empty()) break;
-        cout<<"Green light for road A"<<endl;
-        while(!cars.empty() && directions.front() <=2)
-        {
-            cout<<"Car "<< cars.front() <<" Moving in direction "<< directions.front()
-            <<" Time: "<<time.front() << endl;
-            cars.pop_front();
-            directions.pop_front();
-            time.pop_front();
-        }
-    }
+    char roadName;
+    if(roadId == 0)roadName = 'A';
+    else roadName = 'B';
+
+    if(svetafor != roadId)cout << "Turn Green for road" << roadName << endl;
+    svetafor = roadId;
 }
-void roadB( deque <int> &cars, deque  <int> &directions,deque  <int> &time)
+void carArrived(int carId, int roadId, int direction)
 {
-    while(true)
-    {
-        sleep(1);
-        lock_guard<mutex> guard(m);
-        if(cars.empty()) break;
-        cout<<"Green light for road B"<<endl;
-        while(!cars.empty() && directions.front() >2)
-        {
-            cout<<"Car "<< cars.front() <<" Moving in direction "<< directions.front()
-            <<" Time: "<<time.front() << endl;
-            cars.pop_front();
-            directions.pop_front();
-            time.pop_front();
-        }
-    }
+    char roadName;
+    if(roadId == 0)roadName = 'A';
+    else roadName = 'B';
+
+    cout << "Car = " << carId << " passed road "
+     << roadName << " in direction " << direction << endl;
 }
 int main()
 {
@@ -47,15 +33,26 @@ int main()
     deque  <int> directions = {2,3,1,3,4};
     deque  <int> time = {10,20,30,40,40};
 
-    thread A(roadA,ref(cars),ref(directions),ref(time));
-    thread B(roadB,ref(cars),ref(directions),ref(time));
-    if(A.joinable())
-        A.join();
-    if(B.joinable())
-        B.join();
+    vector<thread>threads;
 
+    for(int i = 0; i < cars.size(); i++)
+    {
+        if(i == 0)usleep(time[i] * 100000); // 1000000ms = 1s;
+        else usleep((time[i] - time[i - 1]) * 100000); // 1000000ms = 1s
 
+        int roadId;
+
+        if(directions[i] <= 2)roadId = 0; // road A
+        else roadId = 1; // road B
+
+        turnGreen(roadId);
+
+        threads.emplace_back(carArrived, cars[i], roadId, directions[i]);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
     return 0;
 }
-
 
